54e544ec4855bc5bac589039bfeabce8e4e6cc37
Vladimir Korsun <korsun.vladimir@gmail.com>
2012-12-14 22:56:56 +0400

    Improve fab.

--- fabfile.py
+++ fabfile.py
@@ -2,27 +2,70 @@ from fabric.api import *
 
 PROJECT_NAME = 'aor'
 
-PROJECT_BASEDIR = '/home/zeus/webapps/%s' % PROJECT_NAME
-PROJECT_ROOT = '/home/zeus/webapps/%s/%s' % (PROJECT_NAME, PROJECT_NAME)
-PROJECT_SOURCE = 'ssh://hg@bitbucket.org/amigo/%s' % PROJECT_NAME
+PROJECT_BASEDIR = '~/webapps/%s' % PROJECT_NAME
+PROJECT_ROOT = '~/webapps/%s/%s' % (PROJECT_NAME, PROJECT_NAME)
+PROJECT_SOURCE = 'https://amigo@bitbucket.org/amigo/%s' % PROJECT_NAME
 
-env.hosts = ['zeus@zeus.webfactional.com']
+env.hosts = ['zeus@zeus.webfactional.com', 'amigo@amigo.webfactional.com']
 
 
-def install():
+def clone_project():
     with cd(PROJECT_BASEDIR):
         run('hg clone %s' % PROJECT_SOURCE)
-        run('virtualenv --clear --no-site-packages env')
-        run('./env/bin/pip install -r %s/build/pipreq.txt' % PROJECT_NAME)
-        run('./env/bin/python manage.py syncdb --all')
-        run('./env/bin/python manage.py collectstatic --noinput')
-
-
-def fu():
+def update_project():
     with cd(PROJECT_ROOT):
         run('hg pull')
         run('hg update -C default')
-        run('../env/bin/python manage.py syncdb')
-        run('../env/bin/python manage.py migrate')
+
+def setup_env():
+    with cd(PROJECT_BASEDIR):
+        run('virtualenv --clear --no-site-packages env')
+
+def update_env():
+    with cd(PROJECT_BASEDIR):
+        run('./env/bin/pip install -U -r %s/build/pipreq.txt' % PROJECT_NAME)
+
+def syncdb():
+    with cd(PROJECT_ROOT):
+        run('../env/bin/python manage.py syncdb --migrate')
+
+def syncdb_all():
+    with cd(PROJECT_ROOT):
+        run('../env/bin/python manage.py syncdb --all')
+
+def collect_static():
+    with cd(PROJECT_ROOT):
         run('../env/bin/python manage.py collectstatic --noinput')
-        run('kill -HUP `head -n 1 %s/aor.pid`' % PROJECT_ROOT)
+
+def start():
+    with cd(PROJECT_ROOT):
+        run('../env/bin/python manage.py run_gunicorn -c gunicorn.conf.py')
+
+def restart():
+    with cd(PROJECT_BASEDIR):
+        run('kill -HUP `cat aor.pid`')
+
+def stop():
+    with cd(PROJECT_BASEDIR):
+        run('kill `cat aor.pid`')
+
+def put_settings():
+    put('aor/settings_local.py', '~/webapps/aor/aor/local.py')
+
+def get_settings():
+    get('~/webapps/aor/aor/local.py', 'aor/settings_local.py')
+
+def install():
+    clone_project()
+    setup_env()
+    update_env()
+    put_settings()
+    syncdb_all()
+    collect_static()
+
+def update():
+    stop()
+    update_project()
+    syncdb()
+    collect_static()
+    start()
