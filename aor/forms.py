c03e6e8de4387625d3aeaf78701898a43558fa0a
Vladimir Korsun <korsun.vladimir@gmail.com>
2012-12-15 18:19:13 +0400

    Custom profile form.

--- aor/forms.py
+++ aor/forms.py
@@ -1,12 +1,20 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django import forms
 from django.utils.translation import ugettext as _
+from pybb.forms import EditProfileForm
 from registration.forms import RegistrationFormUniqueEmail
 from captcha.fields import CaptchaField
+
 
 class RegistrationFormCaptcha(RegistrationFormUniqueEmail):
     captcha = CaptchaField(label=_('Captcha'),
         help_text=_('Enter text from captcha image'))
 
+
 class AuthenticationFormCaptcha(AuthenticationForm):
     captcha = CaptchaField(label=_('Captcha'),
         help_text=_('Enter text from captcha image'))
+
+
+class AORProfileForm(EditProfileForm):
+    signature = forms.CharField(widget=forms.Textarea, label=_('Signature'))
