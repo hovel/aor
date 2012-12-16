08bd366a9d68d2a354177047202567f77ef87a72
Vladimir Korsun <korsun.vladimir@gmail.com>
2012-12-16 22:49:07 +0400

    Extend news.

--- news/forms.py
+++ news/forms.py
@@ -5,4 +5,4 @@ from news.models import News
 class NewsForm(forms.ModelForm):
     class Meta:
         model = News
-        fields = ('title', 'text', 'source')
+        fields = ('title', 'text', 'source', 'author', 'date')
