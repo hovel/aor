62778172e2d0efaee75ad8eb86ada60c3a6dcff8
Vladimir Korsun <korsun.vladimir@gmail.com>
2012-12-21 00:50:08 +0400

    is_authenticated() is method.

--- profiles/context_processor.py
+++ profiles/context_processor.py
@@ -1,6 +1,5 @@
 def user_theme(request):
-    if request.user.is_authenticated:
+    AOR_THEME = 'default'
+    if request.user.is_authenticated():
         AOR_THEME = request.user.get_profile().theme
-    else:
-        AOR_THEME = 'default'
     return dict(AOR_THEME=AOR_THEME)
