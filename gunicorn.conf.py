56ed0a4d28641d7326a0cc637e38025b909ced4f
Vladimir Korsun <korsun.vladimir@gmail.com>
2012-12-24 22:01:49 +0400

    Up workers.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 bind = "127.0.0.1:8002"
 max_requests = 1024
 timeout = 15
-workers = 2
+workers = 5
 daemon = True
 accesslog = '/home/amigo/archlinux/log/access.log'
 errorlog = '/home/amigo/archlinux/log/error.log'
