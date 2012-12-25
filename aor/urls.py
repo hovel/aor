8d3bbea1c7012ef85b29ed28c9482773462a01cd
Vladimir Korsun <korsun.vladimir@gmail.com>
2012-12-26 00:10:21 +0400

    User posts, topics. Improve breadcrumb.

--- aor/urls.py
+++ aor/urls.py
@@ -7,14 +7,15 @@ from pybb.views import ProfileEditView
 from registration.views import register
 from aor.forms import AORProfileForm, RegistrationFormCaptcha
 from aor.views import Search, LastTopics
+from profiles.views import UserTopics, UserPosts
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'),
         name='front-page'),
-#    url(r'^accounts/login/$', login, name="auth_login",
-#        kwargs=dict(authentication_form=AuthenticationFormCaptcha)),
+    #    url(r'^accounts/login/$', login, name="auth_login",
+    #        kwargs=dict(authentication_form=AuthenticationFormCaptcha)),
     url(r'^accounts/register/$', register,
         kwargs=dict(form_class=RegistrationFormCaptcha,
             backend='registration.backends.default.DefaultBackend'),
@@ -23,13 +24,16 @@ urlpatterns = patterns('',
     url(r'^forum/profile/edit/$',
         ProfileEditView.as_view(form_class=AORProfileForm),
         name='pybb:edit_profile'),
+    url(r'^forum/users/(?P<username>[^/]+)/topics/$', UserTopics.as_view(),
+        name='user_topics'),
+    url(r'^forum/users/(?P<username>[^/]+)/posts/$', UserPosts.as_view(),
+        name='user_posts'),
     url(r'^forum/topic/latest/$', LastTopics.as_view(), name='latest'),
     url(r'^forum/', include('pybb.urls', namespace='pybb')),
     url(r'^search/$', Search.as_view(), name='search'),
-    #    url(r'^news/', include('news.urls', namespace='news')),
     url(r'^news/', include('pybb4news.urls', namespace='news')),
     url(r'^blogs/', include('pybb4blogs.urls', namespace='blogs')),
-    url(r'^comments/', include('django.contrib.comments.urls')),
+    #    url(r'^comments/', include('django.contrib.comments.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- profiles/views.py
+++ profiles/views.py
@@ -1,1 +1,55 @@
-# Create your views here.
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.views import generic
+from pure_pagination import Paginator
+from pybb import defaults
+from pybb.models import Post, Topic
+
+
+class UserPosts(generic.ListView):
+    model = Post
+    paginate_by = defaults.PYBB_TOPIC_PAGE_SIZE
+    paginator_class = Paginator
+    template_name = 'pybb/user_posts.html'
+
+    def dispatch(self, request, username, *args, **kwargs):
+        self.user = get_object_or_404(User, username=username)
+        return super(UserPosts, self).dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        qs = super(UserPosts, self).get_queryset()
+        qs = qs.filter(user=self.user)
+        qs = qs.filter(on_moderation=False)
+        qs = qs.filter(topic__forum__hidden=False)
+        qs = qs.order_by('-created', '-updated')
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super(UserPosts, self).get_context_data(**kwargs)
+        context['target'] = self.user
+        return context
+
+
+class UserTopics(generic.ListView):
+    model = Topic
+    paginate_by = defaults.PYBB_FORUM_PAGE_SIZE
+    paginator_class = Paginator
+    template_name = 'pybb/user_topics.html'
+
+    def dispatch(self, request, username, *args, **kwargs):
+        self.user = get_object_or_404(User, username=username)
+        return super(UserTopics, self).dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        qs = super(UserTopics, self).get_queryset()
+        qs = qs.filter(user=self.user)
+        qs = qs.filter(on_moderation=False)
+        qs = qs.filter(forum__hidden=False)
+        qs = qs.order_by('-updated', '-created')
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super(UserTopics, self).get_context_data(**kwargs)
+        context['target'] = self.user
+        return context
+
