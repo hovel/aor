749a242487cc16530ca3619d6bb17b6b4056f998
Vladimir Korsun <korsun.vladimir@gmail.com>
2012-12-26 16:11:52 +0400

    Theme full path.

--- profiles/context_processor.py
+++ profiles/context_processor.py
@@ -1,5 +1,5 @@
 def user_theme(request):
-    AOR_THEME = 'default'
+    AOR_THEME = 'default.less'
     if request.user.is_authenticated():
-        AOR_THEME = request.user.get_profile().theme
+        AOR_THEME = request.user.get_profile().theme + '.less'
     return dict(AOR_THEME=AOR_THEME)
