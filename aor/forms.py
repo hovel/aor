0a1beb5c876e02571347ab725a2b3fe38efe935e
Sergey Fursov <geyser85@gmail.com>
2013-05-26 21:58:08 +0400

    move post improvements

--- aor/forms.py
+++ aor/forms.py
@@ -37,8 +37,8 @@ class MovePostForm(forms.ModelForm):
     def __init__(self, user, *args, **kwargs):
         super(MovePostForm, self).__init__(*args, **kwargs)
         self.fields['topic'].required = True
-        self.fields['topic'].queryset = perms.filter_topics(user, Topic.objects.all()).select_related('forum')\
-            .order_by('forum', 'forum__name', 'name')
+        self.fields['topic'].queryset = perms.filter_topics(user, Topic.objects.filter(closed=False))\
+            .select_related('forum').order_by('forum', 'forum__name', 'name')
 
     class Meta:
         model = Post
