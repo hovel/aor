b1f4cc105856de03d2fe0b2f633d4342bd9e7b01
Sergey Fursov <GeyseR85@gmail.com>
2014-10-22 20:23:23 +0400

    exclude 172.* ips from remote ip addresses

--- aor/middleware.py
+++ aor/middleware.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 class RemoteAddrMiddleware(object):
     def process_request(self, request):
         remote_addr = request.META.get('REMOTE_ADDR')
-        if not remote_addr or remote_addr == '127.0.0.1':
+        if not remote_addr or remote_addr == '127.0.0.1' or remote_addr.startswith('172.'):
             forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
             if not forwarded_for:
                 return
