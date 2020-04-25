e969619fe74415444511acbf02a6442ecef9d2f3
lampslave <lampslave@gmail.com>
2020-04-25 15:30:17 +0300

    fix RemoteAddrMiddleware

--- aor/middleware.py
+++ aor/middleware.py
@@ -10,17 +10,14 @@ class RemoteAddrMiddleware(object):
         super(RemoteAddrMiddleware, self).__init__()
 
     def __call__(self, request):
-        REWRITE_CF_HOST = getattr(settings, 'REWRITE_CF_HOST', None)
-        CF_HOST = getattr(settings, 'CF_HOST', None)
-        if REWRITE_CF_HOST and CF_HOST and request.get_host() == REWRITE_CF_HOST:
-            forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-            if not forwarded_for:
-                return
-            try:
-                remote_addr = forwarded_for.split(',')[0].strip()
-                request.META['REMOTE_ADDR'] = remote_addr
-            except IndexError as e:
-                pass
+        variants = [request.META.get('REMOTE_ADDR', '')]
+        variants.extend(
+            addr.strip() for addr in
+            request.META.get('HTTP_X_FORWARDED_FOR', '').split(','))
+        for v in variants:
+            if v and not v.startswith(('127', '172')):  # localhost, docker
+                request.META['REMOTE_ADDR'] = v
+                break
 
         response = self.get_response(request)
 
